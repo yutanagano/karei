7ca91489cd30e2bb3718a788be1b69e1de7dc949
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-11-09 21:55:25 +0000

    Building a position from a fen now returns a Result<Position, &str>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use karei::Position;
 
 fn main() {
-    println!("Hello, world!");
     let position = Position::new();
     position.print();
 }
--- src/position.rs
+++ src/position.rs
@@ -8,23 +8,23 @@ use std::io::{self, Write};
 pub struct Position {
     board: Board,
     active_color: Color,
-    moves_played: u8,
-    plies_since_last_capture_or_pawn_advance: u8,
+    num_moves_played: u8,
+    num_plies_since_last_capture_or_pawn_advance: u8,
     castling_rights: CastlingRights
 }
 
 impl Position {
     pub fn new() -> Self {
-        Position::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        Position::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap()
     }
 
-    pub fn from_fen(fen: &str) -> Self {
+    pub fn from_fen(fen: &str) -> Result<Self, &str> {
         let mut board = Board::new();
         let mut fen_pieces = fen.split_whitespace();
 
         let piece_placement = match fen_pieces.next() {
             Some(s) => s,
-            _ => panic!("Invalid FEN: Can't read piece placements.")
+            _ => return Err("Bad FEN: empty string.")
         };
 
         let mut square_index: usize = 0;
@@ -40,7 +40,7 @@ impl Position {
                     '7' => square_index += 7,
                     '8' => square_index += 8,
                     '/' => {},
-                    _ => panic!("Unrecognized piece character: {char}")
+                    _ => return Err("Bad FEN: malformed piece placements.")
                 };
                 continue;
             };
@@ -52,7 +52,7 @@ impl Position {
                 'B' => PieceType::Bishop,
                 'N' => PieceType::Knight,
                 'R' => PieceType::Rook,
-                _ => panic!("Unrecognized piece character: {char}")
+                _ => return Err("Bad FEN: malformed piece placements.")
             };
 
             let color = match char.is_ascii_uppercase() {
@@ -71,14 +71,14 @@ impl Position {
             Some(s) => match s {
                 "w" => Color::White,
                 "b" => Color::Black,
-                _ => panic!("Invalid FEN: Unrecognized color designation: {}", s)
+                _ => return Err("Bad FEN: unrecognized active color.")
             },
-            _ => panic!("Invalid FEN: Can't read active color.")
+            _ => return Err("Bad FEN: incomplete.")
         };
 
         let castling_info_string = match fen_pieces.next() {
             Some(s) => s,
-            _ => panic!("Invalid FEN: Can't read castling rights info.")
+            _ => return Err("Bad FEN: incomplete.")
         };
         let mut castling_rights = CastlingRights::new_assuming_all_false();
         for char in castling_info_string.chars() {
@@ -88,7 +88,7 @@ impl Position {
                 'k' => castling_rights[Color::Black].kingside = true,
                 'q' => castling_rights[Color::Black].queenside = true,
                 '-' => {},
-                _ => panic!("Invalid FEN: Unrecognized castling right character: {}.", char)
+                _ => return Err("Bad FEN: malformed castling rights.")
             }
         };
 
@@ -97,7 +97,7 @@ impl Position {
                 "-" => None,
                 coords => Some(Coordinate::try_from(coords).unwrap())
             },
-            None => panic!("Invalid FEN: Missing en passant info.")
+            None => return Err("Bad FEN: incomplete.")
         };
         if let Some(coord) = en_passant_square {
             board[coord].set_en_passant();
@@ -105,21 +105,21 @@ impl Position {
 
         let plies_since_last_capture_or_pawn_advance: u8 = match fen_pieces.next() {
             Some(s) => s.parse().unwrap(),
-            None => panic!("Invalid FEN: Missing ply info.")
+            None => return Err("Bad FEN: incomplete.")
         };
 
         let moves_played: u8 = match fen_pieces.next() {
             Some(s) => s.parse().unwrap(),
-            None => panic!("Invalid FEN: Missing move count info.")
+            None => return Err("Bad FEN: incomplete.")
         };
 
-        Position {
+        Ok(Position {
             board,
             active_color,
-            moves_played,
-            plies_since_last_capture_or_pawn_advance,
+            num_moves_played: moves_played,
+            num_plies_since_last_capture_or_pawn_advance: plies_since_last_capture_or_pawn_advance,
             castling_rights,
-        }
+        })
     }
 
     pub fn print(&self) {
